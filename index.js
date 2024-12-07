1ccc9cff4d2b1fa0a5149812cd662db6de216e42
Sadiyahiru <sadees913@gmail.com>
2024-12-07 23:01:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -162,6 +162,14 @@ if(isReact) return
 m.react("💗")
 }
 //=====================✓
+
+if (config.AUTO_VOICE === 'true') {    
+const url = 'https://raw.githubusercontent.com/Nadeenpoorna-max/NADEENMD_DATABASED/main/autovoice/NADEEN-DATA'
+let { data } = await axios.get(url)
+for (vr in data){
+if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
+ }}
+//====================================//
 const events = require('./command')
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
 if (isCmd) {
