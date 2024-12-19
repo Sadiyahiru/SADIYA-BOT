f9776a1263ce7a55324d56a26f6de62675660a94
Sadiyahiru <sadees913@gmail.com>
2024-12-19 10:27:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "6yYXEJqZ#X236JjtRb5AmRU_AqycESurj-UuQhPMxkssPQJcil5c", //put your session id
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/stxzru.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/wdwNN0R/0cc16fb80f7be4f3.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am sadeesha hirushan*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
 MODE: process.env.MODE || "public", //private or public
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
 };
